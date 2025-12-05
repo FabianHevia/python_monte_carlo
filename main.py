3ad6f437479b1abcfd24c81bb34667aa42efa56e
Fabi <141791420+FabianHevia@users.noreply.github.com>
2025-12-04 22:22:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ Calcula 10.000 escenarios de ganancia/pérdida con volatilidad simple.
 
 # Código
 
+# Importamos random
 import random
 
+# Definimos una caja, una volatibilidad simple y los escenarios
 inicial = 1000
 vol = 0.03
 escenarios = 10000
 
+# Creamos lista de resultados
 resultados = []
 
 for _ in range(escenarios):
+    # Por cada escenario realizar un ejercicio de perdida y ganancia maxima
     cambio = random.uniform(-vol, vol)
     resultados.append(inicial * (1 + cambio))
 
